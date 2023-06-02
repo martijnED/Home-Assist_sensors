91426343f4b2c682140c56ddf4d6639701609dd5
martijn <martijnvwezel@muino.nl>
2023-06-03 01:31:08 +0200

    Version with max and a min function that automatically adjust when

--- Watersensor/src/water-meter.ino
+++ Watersensor/src/water-meter.ino
@@ -20,15 +20,12 @@
 #include <math.h>
 #include "watermeter.h"
 
-// * finding lowest value
-#define ALPHA_COR 0.01 // Value between 0-1
 
 
 
+
+// Keep track amount of msgs
 int sender = 0;
-
-
-
 
 
 struct state_t
@@ -37,30 +34,35 @@ struct state_t
     int8_t fine = 0;
     int32_t liters = 0;
 
-    int16_t a_min = 0;
-    int16_t b_min = 0;
-    int16_t c_min = 0;
+    int16_t a_min = 2500;
+    int16_t b_min = 2500;
+    int16_t c_min = 2500;
 
     int16_t a_max = 0;
     int16_t b_max = 0;
     int16_t c_max = 0;
 };
 
+// * finding lowest value
+// #define ALPHA_COR 0.01 // Value between 0-1 // 0.01 = 1/100 where rolling average shows very smooth graph
 
 
+float mini_average(float x, float y, float alpha_cor) {
 
-
-
-
-
-float mini_average(float x, float y) {
     if ((x + 5) <= y && y > 10) {
         return x;
     } else {
-        return (1 - ALPHA_COR) * x + ALPHA_COR * y;
+        return (1 - alpha_cor) * x + alpha_cor * y;
     }
 }
 
+float max_average(float x, float y, float alpha_cor) {
+    if ((x - 5) >= y && y < 2500) {
+        return x;
+    } else {
+        return (1 - alpha_cor) * x + alpha_cor * y;
+    }
+}
 
 bool not_inited = true;
 static state_t state;
@@ -76,21 +78,27 @@ int magic_code_box(int16_t sen_a, int16_t sen_b, int16_t sen_c) {
         state.b_min = sen_b;
         state.c_min = sen_c;
 
-        state.a_max = sen_a + 50;
-        state.b_max = sen_b + 50;
-        state.c_max = sen_c + 50;
+        state.a_max = 0;
+        state.b_max = 0;
+        state.c_max = 0;
         not_inited      = false;
     }
 
-    if(mili_liters_total < 1){
-        // * Calculate minimum value
-        state.a_min = mini_average(state.a_min, sen_a);
-        state.b_min = mini_average(state.b_min, sen_b);
-        state.c_min = mini_average(state.c_min, sen_c);
+    float alpha_cor  = 0.01;
+    if(mili_liters_total < 2){
+        alpha_cor = 0.1; // when 2 liter not found correct harder
     }
+        // * Calculate minimum value
+        state.a_min = mini_average(state.a_min, sen_a, alpha_cor);
+        state.b_min = mini_average(state.b_min, sen_b, alpha_cor);
+        state.c_min = mini_average(state.c_min, sen_c, alpha_cor);
+
+        state.a_max = max_average(state.a_max, sen_a, alpha_cor);
+        state.b_max = max_average(state.b_max, sen_b, alpha_cor);
+        state.c_max = max_average(state.c_max, sen_c, alpha_cor);
+    // }
 
 
-    int amp = 25;
     int a_zc = (state.a_min + state.a_max) >> 1;
     int b_zc = (state.b_min + state.b_max) >> 1;
     int c_zc = (state.c_min + state.c_max) >> 1;
@@ -108,9 +116,9 @@ int magic_code_box(int16_t sen_a, int16_t sen_b, int16_t sen_c) {
 
     uint32_t mililiters = (uint32_t)(liters_float_fine*1000);
 
-    if (mili_liters_total < mililiters){
+    // if (mili_liters_total < mililiters){
         mili_liters_total = mililiters;
-    }
+    // }
 
     return 1;
 }
@@ -144,14 +152,14 @@ void magnitude_offset_iter(struct state_t *state, int16_t a, int16_t b, int16_t 
         if  ((state->fine > 14) || state->fine < 3)
             *u[phase] = (((((*u[phase]) << SMOOTHING_FACTOR) - (*u[phase]) + *signal[phase]) >> SMOOTHING_FACTOR) + 1 - (phase & 1));
     }
-    else{
-    *a_min = a < *a_min ? (*a_min*4+a)/5 : *a_min;
-    *b_min = b < *b_min ? (*b_min*4+b)/5 : *b_min;
-    *c_min = c < *c_min ? (*c_min*4+c)/5 : *c_min;
-    *a_max = *a_min + 100;
-    *b_max = *b_min + 100;
-    *c_max = *c_min + 100;
-    }
+    // else{
+    // *a_min = a < *a_min ? (*a_min*4+a)/5 : *a_min;
+    // *b_min = b < *b_min ? (*b_min*4+b)/5 : *b_min;
+    // *c_min = c < *c_min ? (*c_min*4+c)/5 : *c_min;
+    // *a_max = *a_min + 100;
+    // *b_max = *b_min + 100;
+    // *c_max = *c_min + 100;
+    // }
 }
 
 // check with three 2pi/3 steps peak autocorrelation and adjust towards max by pi/3 steps
@@ -231,27 +239,35 @@ void do_water_measurment() {
 
 
 
-    // Serial.print(esp_timer_get_time());
-    // Serial.print("\t");
-    // Serial.print(sen_a-sen_a_zero);
-    // Serial.print("\t");
-    // Serial.print(sen_b-sen_b_zero);
-    // Serial.print("\t");
-    // Serial.print(sen_c-sen_c_zero);
+    Serial.print(esp_timer_get_time());
+    Serial.print("\t");
+    Serial.print(sen_a-sen_a_zero);
+    Serial.print("\t");
+    Serial.print(sen_b-sen_b_zero);
+    Serial.print("\t");
+    Serial.print(sen_c-sen_c_zero);
 
-    // Serial.print("\t");
-    // Serial.print(state.a_min);
-    // Serial.print("\t");
-    // Serial.print(state.b_min);
-    // Serial.print("\t");
-    // Serial.print(state.c_min);
+    Serial.print("\t");
+    Serial.print(state.a_min);
+    Serial.print("\t");
+    Serial.print(state.b_min);
+    Serial.print("\t");
+    Serial.print(state.c_min);
 
-    // Serial.print("\t");
-    // Serial.print(state.phase);
-    // Serial.print("\t");
-    // Serial.print(state.liters);
-    // Serial.print("\t");
-    // Serial.println(mili_liters_total);
+    Serial.print("\t");
+    Serial.print(state.a_max);
+    Serial.print("\t");
+    Serial.print(state.b_max);
+    Serial.print("\t");
+    Serial.print(state.c_max);
+
+
+    Serial.print("\t");
+    Serial.print(state.phase);
+    Serial.print("\t");
+    Serial.print(state.liters);
+    Serial.print("\t");
+    Serial.println(mili_liters_total);
 
     sen_a = sen_a - sen_a_zero;
     sen_b = sen_b - sen_b_zero;
@@ -260,7 +276,7 @@ void do_water_measurment() {
 
     if (send && sender++ > 3000) {
         send_data_to_broker();
-        Serial.println(mili_liters_total);
+        // Serial.println(mili_liters_total);
         sender = 0;
     }
     // if (sender % 10 == 0) {
@@ -344,7 +360,7 @@ void setup() {
     mqtt_client.setServer(mqtt_server, mqtt_port);
 
 
-    Serial.println("`time;sen_a;sen_b;sen_c;min_a;min_b;min_c;sen_a_zero;sen_b_zero;sen_c_zero;sen_a_zero;sen_b_zero;sen_c_zero;phase;liters;mili_liters_total");
+    Serial.println("time\tsen_a\tsen_b\tsen_c\tmin_a\tmin_b\tmin_c\tmax_a\tmax_b\tmax_c\tphase\tliters\tmili_liters_total");
     send_data_to_broker();
 }
 
